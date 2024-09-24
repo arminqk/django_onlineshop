ca25733e64750c13d76411634d98d39a7f47af73
Armin <arminghasemi1000@gmail.com>
2024-09-24 17:26:43 +0200

    add login and signup

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = '../templates/home.html'
 
 class AboutUsView(TemplateView):
     template_name = 'pages/aboutus.html'
